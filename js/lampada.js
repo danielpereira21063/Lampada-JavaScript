50ddf8038859391721f03aeaf2fb3a710d79b936
Daniel Sanches <danielsanches6301@gmail.com>
2020-12-06 08:15:37 -0300

    A lâmpada já acende e apaga corretamente

--- js/lampada.js
+++ js/lampada.js
@@ -1,36 +1,49 @@
 const img = new Image();
 const div = document.querySelector('div#container-lamp');
-const lampImg = document.querySelector('img#img');
 const button = document.querySelector('button#turn');
-let broken = false;
+const buttons = {
+    txt: {
+        on: 'Ligar',
+        off: 'Desligar',
+        restart: 'Reiniciar'
+    }
+}
 const lamps = {
-    state: {
-        src: './img/lampada-acesa.jpg',
-        src: './img/lampada-apagada.jpg',
-        src: './img/lampada-quebrada.jpg'
-    },
-    addChild: function(src) {
-        lampImg.src = src;
-        div.appendChild(lampImg);
+    acesa: false,
+    intacta: true, // INFORMACAO SE A LAMPADA ESTÁ QUEBRADA OU NAO
+    off: './img/lampada-apagada.jpg',
+    on: './img/lampada-acesa.jpg',
+    broken: './img/lampada-quebrada.jpg',
+    add: function(src) {
+        img.src = src;
+        div.appendChild(img);
     },
     turn: function() {
-        if(!lampIsBroken()) {
-            
+        if(lamps.intacta) {
+            if(!lamps.acesa) {
+                button.innerHTML = buttons.txt.on;
+                lamps.add(lamps.off);
+                lamps.acesa = true;
+            } else if(lamps.acesa) {
+                button.innerHTML = buttons.txt.off;
+                lamps.add(lamps.on);
+                lamps.acesa = false;
+            }
+        }
+    },
+    lampIsBroken: function() {
+        if(lamps.broken) {
+            return true;
+        } else {
+            return false;
         }
     }
 };
-function lampIsBroken() {
-    if(broken) {
-        return true;
-    } else {
-        return false;
-    }
-}
 
 button.addEventListener('click', lamps.turn);
 function load() {
-    lamps.state.src = './img/lampada-apagada.jpg';
-    img.src = lamps.state.src;
+    img.src = lamps.off;
     div.appendChild(img);
+    lamps.turn();
 }
 load();
