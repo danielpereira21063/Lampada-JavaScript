5a8b63d1cfd705e5b8d228a6904aa890a8862f48
Daniel Sanches <danielsanches6301@gmail.com>
2020-12-06 21:38:38 -0300

    Ultima atualização estável com todas as funcionalidades

--- js/lampada.js
+++ js/lampada.js
@@ -13,6 +13,7 @@ const lamp = {
         if(!lamp.quebrada) {
             img.src = lamp.src.acesa;
             mudarImagem(img);
+            mudarLampInfo('A lâmpada foi acesa');
             alterarNomeBotao('Apagar');
         }
     },
@@ -21,6 +22,7 @@ const lamp = {
             img.src = lamp.src.apagada;
             mudarImagem(img);
             alterarNomeBotao('Acender');
+            mudarLampInfo('A lâmpada foi apagada');
         }
     },
     quebrar: function() {
@@ -28,9 +30,13 @@ const lamp = {
             lamp.quebrada = true;
             img.src = lamp.src.quebrada;
             mudarImagem(img);
+            mudarLampInfo('A lâmpada foi apagada');
             alterarNomeBotao('Reiniciar');
         }
     }
+}
+function mudarLampInfo(newName) {
+    lampInfo.innerHTML = newName;
 }
 function mudarImagem(img) {
     div.appendChild(img);
@@ -39,10 +45,11 @@ function alterarNomeBotao(newName) {
     button.innerHTML = newName;
 }
 const inicializar = function() {
-    lamp.quebrada = false;
     img.src = './img/lampada-apagada.jpg';
-    alterarNomeBotao('Acender');
     mudarImagem(img);
+    alterarNomeBotao('Acender');
+    mudarLampInfo('A lâmpada está apagada');
+    lamp.quebrada = false;
 }
 div.addEventListener('dblclick', lamp.quebrar);
 button.addEventListener('click', function(){
